09c488ed71c364312211fc5ba5e8933565045435
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-03-27 01:03:05 -0400

    fixed local storage?

--- Develop/script/script.js
+++ Develop/script/script.js
@@ -2,27 +2,32 @@
 var now= dayjs()
 $("#currentDay").text(now.format('MMM D, YYYY, h:mm a'));
 
+var toDoInput = document.querySelector("description");
+
+var toDos = [];
+
+
+function init() {
+  var storedTodos = JSON.parse(localStorage.getItem(toDoInput));
+
+  if (storedTodos !== null) {
+    toDos = storedTodos;
+  }
+};
+
+function storeTodos() {
+  localStorage.setItem("description", JSON.stringify(toDoInput));
+};
+
 //event listener and local storage
 $(document).ready(function () {
   $(".saveBtn").on("click", function(event) {
     event.preventDefault();
-
-      var text = $(this).siblings(".description").val();
-      var time = $(this).parent().attr("id");
-      localStorage.setItem(time, text);  
-
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    
+    var text = $(this).siblings("description").val();
+    
   });
-
+  
   function timeTracker() {
     var timeNow = now;
 
