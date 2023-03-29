a4835fe6ef1e85c38ed8c3ef54b1d703407b0e42
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-03-29 19:28:31 -0400

    updating local storage code for deployment to see if it works on live site.

--- Develop/script/script.js
+++ Develop/script/script.js
@@ -1,6 +1,6 @@
 //Date/Time display
 const storageInput = document.querySelector('.description');
-const button = document.querySelector('.saveBtn');
+const button = document.querySelector('.fa-save');
 
 var now= dayjs()
 $("#currentDay").text(now.format('MMM D, YYYY, h:mm a'));
@@ -13,6 +13,17 @@ $(document).ready(function () {
   $(button).on("click", function() {    
     if($(storageInput).val() !== ""){
       localStorage.setItem(storageInput).val();
+        $("#hour8 .description").val(localStorage.getItem("hour8"));
+        $("#hour9 .description").val(localStorage.getItem("hour9"));
+        $("#hour10 .description").val(localStorage.getItem("hour10"));
+        $("#hour11 .description").val(localStorage.getItem("hour11"));
+        $("#hour12 .description").val(localStorage.getItem("hour12"));
+        $("#hour13 .description").val(localStorage.getItem("hour13"));
+        $("#hour14 .description").val(localStorage.getItem("hour14"));
+        $("#hour15 .description").val(localStorage.getItem("hour15"));
+        $("#hour16 .description").val(localStorage.getItem("hour16"));
+        $("#hour17 .description").val(localStorage.getItem("hour17"));
+        $("#hour18 .description").val(localStorage.getItem("hour18"));
 
       alert("Schedule Saved!");
     }
